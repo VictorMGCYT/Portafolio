a60c82c209caa22d4c8a12b3b66081c9934a43bc
VictorMGCYT <manuelvgc2233@gmail.com>
2024-12-05 21:24:20 -0600

    formulario funcionando

--- scripts/scriptIndex.js
+++ scripts/scriptIndex.js
@@ -6,3 +6,28 @@ document.getElementById('about').addEventListener('click', function () {
         block: 'center'      // Alinea la parte superior del elemento con la parte superior de la ventana
     });
 })
+
+
+
+
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'service_fcp6paq';
+   const templateID = 'template_w47gskl';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Correo enviado';
+      alert('¡Enviado!');
+    }, (err) => {
+      btn.value = 'Correo enviado';
+      alert(JSON.stringify(err));
+    });
+});
